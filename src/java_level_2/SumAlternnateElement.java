83190c0e29b93a73872a3117d8d20cd1fd1caf64
sarojkaka <sarojkaka@gmail.com>
2022-10-11 23:30:28 -0400

    Create SumAlternnateElement.java

---
+++ src/java_level_2/SumAlternnateElement.java
@@ -0,0 +1,33 @@
+package java_level_2;
+import java.util.Arrays;
+
+public class SumAlternnateElement {
+
+	public static void main(String[] args) {
+
+		int[] num = { 1, 2, 3, 4, 5, 6,7 };
+		System.out.println("Elements of the array are : " + Arrays.toString(num));
+		int sum = 0;
+		int sum1 = 0;
+		for (int i = 0; i < num.length; i = i + 2) {
+			sum = sum + num[i];
+		}
+		for (int j = 1; j < num.length; j = j + 2) {
+			System.out.println("Alternative Element : " + j);
+		}
+		System.out.println("   Sum of the Elements:" + sum);
+
+		{
+			for (int x = 1; x < num.length; x = x + 2) {
+				sum1 = sum1 + num[x];
+			}
+			for (int y = 2; y < num.length; y = y + 2) {
+				System.out.println("Alternative Element : " + y);
+			}
+			System.out.println("   Sum of the Elements:" + sum1);
+		}
+
+	}
+
+}
+
