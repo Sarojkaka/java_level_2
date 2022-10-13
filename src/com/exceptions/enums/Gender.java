1597b1ad1fabd0de074cfd97e86b5a41c888458a
sarojkaka <sarojkaka@gmail.com>
2022-10-13 17:15:13 -0400

    Create Gender.java

---
+++ src/com/exceptions/enums/Gender.java
@@ -0,0 +1,17 @@
+package com.exceptions.enums;
+
+public enum Gender {
+	MALE("M"),
+	OTHERS("O"),
+	FEMALE("F");
+	
+	private String val;
+	
+	Gender(String val) {
+		this.val = val;
+	}
+	
+	public String getValue() {
+		return this.val;
+	}
+}
