95c9764a6a72db7287dae6a157f801b2fe783367
sarojkaka <sarojkaka@gmail.com>
2022-10-12 22:44:04 -0400

    Update nPoweredNumber.java

--- src/java_level_2/nPoweredNumber.java
+++ src/java_level_2/nPoweredNumber.java
@@ -6,11 +6,13 @@ public class nPoweredNumber {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int pnum = sc.nextInt();
 		int[] number = { 256, 1, 2, 3, 4, 5, 6, 7, 512, 8, 9, 10, 16, 32, 64, 128 };
 		for (int i = 0; i < number.length; i++) {
 			for (int j = 1; j < number[i]; j++)
 				
-				if (number[i] == Math.pow(2, j)) {
+				if (number[i] == Math.pow(pnum, j)) {
 
 					System.out.println(number[i]);
 				}
