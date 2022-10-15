080dd664436d08ff29d1045f2c8c73766c9519c9
sarojkaka <sarojkaka@gmail.com>
2022-10-15 13:55:05 -0400

    Create RomanToInt.java

---
+++ src/java_level_2/RomanToInt.java
@@ -0,0 +1,31 @@
+package java_level_2;
+
+import java.util.Scanner;
+
+public class RomanToInt {
+	public static void integerToRoman(int num) {
+
+		System.out.println("Integer: " + num);
+		int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+		String[] romanLiterals = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+		StringBuilder roman = new StringBuilder();
+
+		for (int i = 0; i < values.length; i++) {
+			while (num >= values[i]) {
+				num -= values[i];
+				roman.append(romanLiterals[i]);
+			}
+		}
+		System.out.println("Roman: " + roman.toString());
+	}
+
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter Number");
+			integerToRoman(sc.nextInt());
+		}
+
+	}
+
+}
